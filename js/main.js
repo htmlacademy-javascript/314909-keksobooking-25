80af12f1eed389a72df2496de799a500b03ed793
Trira <trubchik@mail.ru>
2022-02-22 16:09:27 +0300

    изменения в main.js
    5я версия. Учтены undefined параметры в функциях

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 // подключение скрипта  <script src="js/main.js"></script>
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
-    if (min <= 0) { alert('начальное значение диапазона не должно быть отрицательным!') };
-    if (max - min <= 0) { alert('начальное значение диапазона не должно быть больше конечного!') };
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+function getRandomIntInclusive(min, max) {
+  max = max ? Math.ceil(min) : Math.floor(max);
+  min = max ? 0 : Math.ceil(min);
+  const res = min >= 0 && max >= min ? Math.floor(min + Math.random() * (max - min + 1)) : NaN;
+  return res;
 }
 
 /* Функция, возвращающая случайное число с плавающей точкой 
@@ -14,9 +13,13 @@ function getRandomInt(min, max) {
 Будет использоваться для генерации временных географических координат 
 в следующем задании. */
 // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой  (accur)"
-function getRandomArbitrary(min, max, accur) {
-    if (min <= 0) { alert('начальное значение диапазона не должно быть отрицательным!') };
-    if ((max - min) <= 0) { alert('начальное значение диапазона не должно быть больше конечного!') };
-    return Math.round(Math.random() * (max - min + 1) + min, accur);
+function getRandomArbitraryInclusive(min, max, accuracy) {
+  const accur = accuracy ? 0 : accuracy;
+  const res = min >= 0 && max >= min ? +(Math.random() * (max - min) + min).toFixed(accur) : NaN;
+  return res;
 }
+
+getRandomArbitraryInclusive(1.1, 1.2, 0.1);
+getRandomIntInclusive(1, 10);
+
 
