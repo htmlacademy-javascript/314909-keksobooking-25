30f87a76b8c26325dee831ea2b888562e18e0dd9
Irina Trubchik <trubchik@mail.ru>
2022-03-07 17:48:15 +0300

    исправления для 4.9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// подключение скрипта  <script src="js/main.js"></script>
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = [
@@ -14,6 +13,15 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+const DESCRIPTION = [
+  'Описание помещения',
+  'Великолепная квартира-студия',
+  'Гнездышко для молодоженов',
+  'Квартира для студентов',
+  'Для многодетной семьи',
+  'Для одиночек с животными',
+];
+
 const MAX_ADS = 10;
 const MIN_PRICE = 1;
 const MAX_PRICE = 30000;
@@ -25,59 +33,56 @@ const MIN_LAT = 35.65000;
 const MAX_LAT = 35.70000;
 const MIN_LNG = 139.70000;
 const MAX_LNG = 139.80000;
-const FLOAT = 5;
+const COORD_PRECISION = 5;
 
 // Возвращает случайное целое число из переданного диапазона включительно.
-const getRandomNumber = (a, b) => {
-  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+const getRandomNumber = (start, end) => {
+  const min = Math.ceil(Math.min(Math.abs(start), Math.abs(end)));
+  const max = Math.floor(Math.max(Math.abs(start), Math.abs(end)));
   const result = Math.random() * (max - min + 1) + min;
 
   return Math.floor(result);
 };
 
 // Возвращает случайное число с плавающей точкой из переданного диапазона включительно.
-const getRandomFloat = (a, b, digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
+const getRandomFloat = (start, end, digits = 1) => {
+  const lower = Math.min(Math.abs(start), Math.abs(end));
+  const upper = Math.max(Math.abs(start), Math.abs(end));
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
 };
 
 // Вовзращает случайный элемент массива
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
-const getRandomLengthArr = (array) => {
-  const newArr = [];
+const getRandomSlice = (array) => {
   const count = getRandomNumber(1, array.length);
-  for (let i = 0; i < count; i++) {
-    newArr.push(array[i]);
-  }
-  return newArr;
+  return array.slice(0, count + 1);
 };
 
 const createPoint = (i) => {
   const location = {
-    lat: getRandomFloat(MIN_LAT, MAX_LAT, FLOAT),
-    lng: getRandomFloat(MIN_LNG, MAX_LNG, FLOAT),
+    lat: getRandomFloat(MIN_LAT, MAX_LAT, COORD_PRECISION),
+    lng: getRandomFloat(MIN_LNG, MAX_LNG, COORD_PRECISION),
+  };
+  const offer = {
+    title: 'Заголовок предложения',
+    address: `${location.lat}, ${location.lng}`,
+    price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+    guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+    checkin: getRandomArrayElement(TIME),
+    checkout: getRandomArrayElement(TIME),
+    features: getRandomSlice(FEATURES),
+    description: getRandomArrayElement(DESCRIPTION),
+    photos: getRandomSlice(PHOTOS),
   };
   return {
     autor: {
       avatar: `img/avatars/user${0 + i.slice(-2)}.png`
     },
     location,
-    offer: {
-      title: 'Заголовок предложения',
-      address: `${location.lat}, ${location.lng}`,
-      price: getRandomNumber(MIN_PRICE, MAX_PRICE),
-      type: getRandomArrayElement(TYPES),
-      rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
-      guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
-      checkin: getRandomArrayElement(TIME),
-      checkout: getRandomArrayElement(TIME),
-      features: getRandomLengthArr(FEATURES),
-      description: 'Описание помещения',
-      photos: getRandomLengthArr(PHOTOS),
-    }
+    offer
   };
 };
 
