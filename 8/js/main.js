07d31f72d830b78d3014f0eff49cdff2206dc145
Keks <keks@htmlacademy.ru>
2022-04-10 17:35:30 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/*
+    Импорты других модулей
+    Вызовы общих функций
+    Настройка скриптов
+    ...
+*/
+
+import { createPoint } from './setup.js';
+
+console.log(
+  createPoint()
+);
