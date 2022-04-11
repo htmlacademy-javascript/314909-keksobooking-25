761a280b891723cec02ceb05c5225ee4e9e23368
Trira <trubchik@mail.ru>
2022-04-11 23:14:25 +0300

    изменения для задания 7.11

--- js/main.js
+++ js/main.js
@@ -7,8 +7,14 @@
     ...
 */
 
-import { createPoint } from './setup.js';
+import { arrayOfAdverts, ADVERTS_NUMBER } from './setup.js';
+import { drawAd } from './markup-generator.js';
+import { getActiveState, getUnactiveState } from './map-statement.js';
+import './form.js';
+import './map.js';
 
-console.log(
-  createPoint()
-);
+arrayOfAdverts(ADVERTS_NUMBER);
+drawAd();
+
+getUnactiveState();
+setTimeout(getActiveState, 2000);
