f38e688d677e9835b9b62343881b9b2ee27c998c
Irina Trubchik <trubchik@mail.ru>
2022-04-18 20:33:19 +0300

    рендеринг проекта

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-/*
-    Импорты других модулей
-    Вызовы общих функций
-    Настройка скриптов
-    ...
-*/
 
-import { arrayOfAdverts, ADVERTS_NUMBER } from './advertisment';
-import { TIME_OUT } from './data.js';
-import { drawMap } from './draw-map.js';
-import { deactivateForms, activateAdvertForm, activateMapFilterForm } from './map-statement.js';
-import './form.js';
-import './map.js';
+import { deactivateForms } from './page-states.js';
+import { initMap } from './map.js';
 
-arrayOfAdverts(ADVERTS_NUMBER);
-drawMap();
-
-// deactivateForms();
-// activateAdvertForm();
-// setTimeout(activateMapFilterForm(), TIME_OUT);
+deactivateForms();
+initMap();
