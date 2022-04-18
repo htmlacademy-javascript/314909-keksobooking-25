b2202d26f8ba5951ecaa469b644f4b3257fb4835
Trira <trubchik@mail.ru>
2022-04-18 23:02:24 +0300

    Update main.js
    копия локальной рабочей версии

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-/*
-    Импорты других модулей
-    Вызовы общих функций
-    Настройка скриптов
-    ...
-*/
+import { deactivateForms } from './map-statement.js';
+import { createMap } from './map.js';
 
-import { arrayOfAdverts, ADVERTS_NUMBER } from './setup.js';
-import { TIME_OUT } from './data.js';
-import { drawMap } from './map-generator.js';
-import { getActiveState, getUnactiveState } from './map-statement.js';
-import './form.js';
-import './map.js';
-
-arrayOfAdverts(ADVERTS_NUMBER);
-drawMap();
-
-getUnactiveState();
-setTimeout(getActiveState, TIME_OUT);
+deactivateForms();
+createMap();
