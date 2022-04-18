f68e369a1fca4350d040237875659fc1b3519527
Keks <keks@htmlacademy.ru>
2022-04-18 03:16:29 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/*
+    Импорты других модулей
+    Вызовы общих функций
+    Настройка скриптов
+    ...
+*/
+
+import { arrayOfAdverts, ADVERTS_NUMBER } from './setup.js';
+import { TIME_OUT } from './data.js';
+import { drawMap } from './map-generator.js';
+import { getActiveState, getUnactiveState } from './map-statement.js';
+import './form.js';
+import './map.js';
+
+arrayOfAdverts(ADVERTS_NUMBER);
+drawMap();
+
+getUnactiveState();
+setTimeout(getActiveState, TIME_OUT);
