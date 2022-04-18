68eea0418e2b4ce52cc70857f9c0a548c9a7d4c2
Keks <keks@htmlacademy.ru>
2022-04-18 12:24:15 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/api.js
@@ -0,0 +1,50 @@
+/* eslint-disable indent */
+import { onResetButtonClick } from './form-validations.js';
+
+const ERROR_SHOW_TIME = 5000;
+const DATABASE_URL = 'https://25.javascript.pages.academy/keksobooking';
+const DATABSE_OFFERS_URL = `${DATABASE_URL}/data`;
+
+const showError = (error) => {
+    const errorContainer = document.createElement('div');
+    errorContainer.classList.add('error-container');
+    errorContainer.textContent = error;
+
+    document.body.append(errorContainer);
+
+    setTimeout(() => {
+        errorContainer.remove();
+    }, ERROR_SHOW_TIME);
+};
+
+const getData = (onSuccess, onFail) => {
+    fetch(DATABSE_OFFERS_URL)
+        .then((response) => response.json())
+        .then((offers) => {
+            onSuccess(offers);
+        })
+        .catch(onFail);
+};
+
+const sendData = (onSuccess, onFail, body) => {
+    fetch(
+        DATABASE_URL,
+        {
+            method: 'POST',
+            body,
+        },
+    )
+        .then((response) => {
+            if (response.ok) {
+                onSuccess();
+                onResetButtonClick();
+            } else {
+                onFail('Не удалось отправить форму. Попробуйте ещё раз');
+            }
+        })
+        .catch(() => {
+            onFail('Не удалось отправить форму. Попробуйте ещё раз');
+        });
+};
+
+export { getData, sendData, showError };
